d061ce2338652336e284ee26d4ce5d45dd93ebe5
Thariq Zhafran Satyagraha <71017985+thoriqzs25@users.noreply.github.com>
2026-05-14 17:14:20 +0700

    versino 2

--- src/components/FriendGraphApp.tsx
+++ src/components/FriendGraphApp.tsx
@@ -514,7 +514,7 @@ export default function FriendGraphApp() {
         <ConnectForm nodes={snapshot.nodes} onConnect={addLink} />
 
         <div className="mt-auto pt-4 text-center text-[11px] text-zinc-600">
-          v1.0.1
+          v1.0.2
         </div>
       </aside>
 
